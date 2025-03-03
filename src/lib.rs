98376fcd3b8716bf5516de6a798f92b301e4c849
K0enM <github@sl.koenmolenaar.nl>
2025-03-03 01:33:54 +0100

    Article 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,288 @@
+use derive_more::*;
+use derive_try_from_primitive::TryFromPrimitive;
+use enumflags2::*;
+#[allow(unused_imports)]
+use nom::number::complete::{le_u16, le_u32};
+use nom::sequence::tuple;
+use std::fmt;
+use std::fmt::Formatter;
+use std::ops::Range;
 mod parse;
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, TryFromPrimitive)]
+#[repr(u16)]
+pub enum Type {
+    None = 0x0,
+    Rel = 0x1,
+    Exec = 0x2,
+    Dyn = 0x3,
+    Core = 0x4,
+}
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq, TryFromPrimitive)]
+#[repr(u16)]
+pub enum Machine {
+    X86 = 0x03,
+    X86_64 = 0x3e,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, TryFromPrimitive)]
+#[repr(u32)]
+pub enum SegmentType {
+    Null = 0x0,
+    Load = 0x1,
+    Dynamic = 0x2,
+    Interp = 0x3,
+    Note = 0x4,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, BitFlags)]
+#[repr(u32)]
+pub enum SegmentFlag {
+    Execute = 0x1,
+    Write = 0x2,
+    Read = 0x4,
+}
+pub struct ProgramHeader {
+    pub r#type: SegmentType,
+    pub flags: BitFlags<SegmentFlag>,
+    pub offset: Addr,
+    pub vaddr: Addr,
+    pub paddr: Addr,
+    pub filesz: Addr,
+    pub memsz: Addr,
+    pub align: Addr,
+    pub data: Vec<u8>,
+}
+
+#[derive(Debug)]
+pub struct File {
+    pub r#type: Type,
+    pub machine: Machine,
+    pub entry_point: Addr,
+    pub version: u32,
+    pub program_headers: Vec<ProgramHeader>,
+}
+
+pub struct HexDump<'a>(&'a [u8]);
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Add, Sub)]
+pub struct Addr(pub u64);
+
+impl ProgramHeader {
+    pub fn file_range(&self) -> Range<Addr> {
+        self.offset..self.offset + self.filesz
+    }
+
+    /**
+     * Memory range where the segment is mapped
+     */
+    pub fn mem_range(&self) -> Range<Addr> {
+        self.vaddr..self.vaddr + self.memsz
+    }
+
+    fn parse<'a>(full_input: parse::Input<'_>, i: parse::Input<'a>) -> parse::Result<'a, Self> {
+        let (i, (r#type, flags)) = tuple((SegmentType::parse, SegmentFlag::parse))(i)?;
+        let ap = Addr::parse;
+
+        let (i, (offset, vaddr, paddr, filesz, memsz, align)) = tuple((ap, ap, ap, ap, ap, ap))(i)?;
+
+        let res = Self {
+            r#type,
+            flags,
+            offset,
+            vaddr,
+            paddr,
+            filesz,
+            memsz,
+            align,
+            data: full_input[offset.into()..][..filesz.into()].to_vec(),
+        };
+
+        Ok((i, res))
+    }
+}
+
+impl File {
+    const MAGIC: &'static [u8] = &[0x7f, 0x45, 0x4c, 0x46];
+
+    #[allow(unused_variables)]
+    pub fn parse(i: parse::Input) -> parse::Result<Self> {
+        use nom::{
+            bytes::complete::{tag, take},
+            error::context,
+        };
+
+        let full_input = i;
+        let (i, _) = tuple((
+            context("Magic", tag(Self::MAGIC)),
+            context("Class", tag(&[0x2])),
+            context("Endianness", tag(&[0x1])),
+            context("Version", tag(&[0x1])),
+            context("OS ABI", nom::branch::alt((tag(&[0x0]), tag(&[0x3])))),
+            context("Padding", take(8_usize)),
+        ))(i)?;
+
+        let (i, (r#type, machine)) = tuple((Type::parse, Machine::parse))(i)?;
+
+        let (i, version) = le_u32(i)?;
+        let (i, entry_point) = Addr::parse(i)?;
+
+        dbg!(version);
+
+        use nom::{combinator::map, number::complete::le_u16};
+        // some values are stored as u16 to save storage, but they're actually
+        // file offsets, or counts, so we want them as `usize` in rust.
+        let u16_usize = map(le_u16, |x| x as usize);
+
+        // ph = program header, sh = section header
+        let (i, (ph_offset, sh_offset)) = tuple((Addr::parse, Addr::parse))(i)?;
+        let (i, (flags, hdr_size)) = tuple((le_u32, le_u16))(i)?;
+        let (i, (ph_entsize, ph_count)) = tuple((&u16_usize, &u16_usize))(i)?;
+        let (i, (sh_entsize, sh_count, sh_nidx)) = tuple((&u16_usize, &u16_usize, &u16_usize))(i)?;
+
+        // `chunks()` divides a slice into chunks of equal size - perfect, as we know the entry size.
+        let ph_slices = (&full_input[ph_offset.into()..]).chunks(ph_entsize);
+        let mut program_headers = Vec::new();
+        for ph_slice in ph_slices.take(ph_count) {
+            let (_, ph) = ProgramHeader::parse(full_input, ph_slice)?;
+            program_headers.push(ph);
+        }
+
+        let res = Self {
+            machine,
+            r#type,
+            entry_point,
+            version,
+            program_headers,
+        };
+        Ok((i, res))
+    }
+
+    pub fn parse_or_print_error(i: parse::Input) -> Option<Self> {
+        match Self::parse(i) {
+            Ok((_, file)) => Some(file),
+            Err(nom::Err::Failure(err)) | Err(nom::Err::Error(err)) => {
+                eprintln!("Parsing failed:");
+                for (input, err) in err.errors {
+                    use nom::Offset;
+                    let offset = i.offset(input);
+                    eprintln!("{:?} at position {}: ", err, offset);
+                    eprintln!("{:>08x}: {:?}", offset, HexDump(input));
+                }
+                None
+            }
+            Err(_) => panic!("Unexpected nom error"),
+        }
+    }
+}
+
+impl_parse_for_enum!(Type, le_u16);
+impl_parse_for_enum!(Machine, le_u16);
+impl_parse_for_enum!(SegmentType, le_u32);
+impl_parse_for_enumflags!(SegmentFlag, le_u32);
+
+impl<'a> fmt::Debug for HexDump<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        for &x in self.0.iter().take(20) {
+            write!(f, "{:02x}", x)?;
+        }
+        Ok(())
+    }
+}
+impl fmt::Debug for ProgramHeader {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "file {:?} | mem {:?} | align {:?} | {} {:?}",
+            self.file_range(),
+            self.mem_range(),
+            self.align,
+            &[
+                (SegmentFlag::Read, "R"),
+                (SegmentFlag::Write, "W"),
+                (SegmentFlag::Execute, "X"),
+            ]
+            .iter()
+            .map(|&(flag, letter)| {
+                if self.flags.contains(flag) {
+                    letter
+                } else {
+                    "."
+                }
+            })
+            .collect::<Vec<_>>()
+            .join(""),
+            self.r#type
+        )
+    }
+}
+
+impl fmt::Debug for Addr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:08x}", self.0)
+    }
+}
+
+impl fmt::Display for Addr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        fmt::Debug::fmt(self, f)
+    }
+}
+
+// This will come in handy when serializing
+impl Into<u64> for Addr {
+    fn into(self) -> u64 {
+        self.0
+    }
+}
+
+impl Into<usize> for Addr {
+    fn into(self) -> usize {
+        self.0 as usize
+    }
+}
+
+impl From<u64> for Addr {
+    fn from(x: u64) -> Self {
+        Self(x)
+    }
+}
+
+impl Addr {
+    pub fn parse(i: parse::Input) -> parse::Result<Self> {
+        use nom::{combinator::map, number::complete::le_u64};
+        map(le_u64, From::from)(i)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Machine;
+    use std::convert::TryFrom;
+
+    #[test]
+    fn type_to_u16() {
+        assert_eq!(super::Type::Dyn as u16, 0x3);
+    }
+
+    #[test]
+    fn try_enums() {
+        assert_eq!(Machine::X86_64 as u16, 0x3E);
+        assert_eq!(Machine::try_from(0x3E), Ok(Machine::X86_64));
+        assert_eq!(Machine::try_from(0xFA), Err(0xFA));
+    }
+
+    #[test]
+    fn try_bitflag() {
+        use super::SegmentFlag;
+        use enumflags2::BitFlags;
+
+        let flags_integer: u32 = 6;
+        let flags = BitFlags::<SegmentFlag>::from_bits(flags_integer).unwrap();
+        assert_eq!(flags, SegmentFlag::Read | SegmentFlag::Write);
+        assert_eq!(flags.bits(), flags_integer);
+
+        assert!(BitFlags::<SegmentFlag>::from_bits(1992).is_err());
+    }
+}
